c234ec94543777ca8c3818746d9bf665c7334e24
JH <55tggcc@gmail.com>
2024-05-12 21:45:47 +0900

    레디스 연결 테스트 코드 작성

--- app/index.ts
+++ app/index.ts
@@ -1,16 +1,26 @@
 import express from "express";
-
-const app = express()
-
-app.use(express.json())
-
-app.get("/", (request, response) => {
-  response.status(200).send("hello from express")
-})
+import * as redis from 'redis';
 
 const PORT = 4000;
 
-app.listen(PORT, () => {
-  console.log(`App listening at port ${PORT}`)
+const createApp = async () => {
+  const app = express()
+  const client = redis.createClient({url: "redis://localhost:6379"})
+  await client.connect(); // await 사용해야해서 전체 내용을 함수로 감싸주기
+
+  app.use(express.json())
+
+  app.get("/", (request, response) => {
+    response.status(200).send("hello from express")
+  })
+
+  return app;
+}
+
+createApp().then((app) => {
+  app.listen(PORT, () => {
+    console.log(`App listening at port ${PORT}`)
+  })
 })
+
 
