8f7d59b8b809b84fae81973f15bdf9640b77f224
JH <55tggcc@gmail.com>
2024-05-12 18:38:57 +0900

    express 설치 후 get 리퀘스트 날려보기 작성

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,16 @@
-console.log("hello world!!!")
+import express from "express";
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (request, response) => {
+  response.status(200).send("hello from express")
+})
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}`)
+})
+
